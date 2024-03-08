56b1ee86f2544ea4d76045de0e3e1ca686019612
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-08 16:12:13 +0100

    add api key on store.js

--- src/App.vue
+++ src/App.vue
@@ -19,46 +19,35 @@ export default {
   },
 
   created(){
-    const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWM0NWRiMzMyZTQ4NmIzY2UxMzVkYzZiMDg4ZWI3ZSIsInN1YiI6IjYwYTBlNWE4ZTE4ZTNmMDA1OGRlZDE5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qhi4cyNjuuzbIBkmg7c5TKybAN_pLBz6GDbISxPNnQ8'
-  }
-};
-    axios.get("https://api.themoviedb.org/3/movie/popular?", options).then(res => {
+    axios.get("https://api.themoviedb.org/3/movie/popular?", this.store.options).then(res => {
       this.store.movies = res.data.results;
       console.log(res.data.results);
     });
 
-    axios.get("https://api.themoviedb.org/3/tv/popular?", options).then(res => {
+    axios.get("https://api.themoviedb.org/3/tv/popular?", this.store.options).then(res => {
       this.store.series = res.data.results;
       console.log(res.data.results);
     });
 
-
-// call for single movie cast
-axios.get('https://api.themoviedb.org/3/movie/196/credits', options)
-  .then(res => {
-  console.log(res.data.cast);
-  }).catch(err => console.error(err));
+  // call for single movie cast
+  axios.get('https://api.themoviedb.org/3/movie/196/credits', this.store.options)
+    .then(res => {
+    console.log(res.data.cast);
+    }).catch(err => console.error(err));
 
 
   // call for movie genres
-  axios('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
-  .then(res => {
+  axios('https://api.themoviedb.org/3/genre/movie/list?language=en', this.store.options)
+    .then(res => {
     console.log(res.data.genres);
-  }).catch(err => console.error(err));
+    }).catch(err => console.error(err));
 
     // call for series genres
-  axios('https://api.themoviedb.org/3/genre/tv/list?language=en', options)
-  .then(res => {
+  axios('https://api.themoviedb.org/3/genre/tv/list?language=en', this.store.options)
+    .then(res => {
     console.log(res.data.genres);
   }).catch(err => console.error(err));
-
-
-  
-  },
+},
 
 methods:{
 
--- src/store.js
+++ src/store.js
@@ -17,4 +17,12 @@ export const store = reactive({
     casts: [],
 
 
+    options: {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer 19c45db332e486b3ce135dc6b088eb7e'
+        }
+    },
+
 });
